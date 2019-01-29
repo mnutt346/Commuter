e1cbd5910e70481aabb1050648bbeacc5f03601c
Michael Nutt <mnutt346@gmail.com>
2019-01-28 20:04:12 -0600

    Build out Log In page.

--- database/models.js
+++ database/models.js
@@ -3,22 +3,12 @@ mongoose.Promise = require("bluebird");
 
 const UsersSchema = new mongoose.Schema(
   {
-    username: String,
+    email: String,
     password: String,
     home: String,
     work: String
   },
   { collection: "userInfo" }
 );
-
-// generating a hash
-UsersSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-UsersSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
 
 module.exports = mongoose.model("userInfo", UsersSchema);
